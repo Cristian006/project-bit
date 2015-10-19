b9ba5ef04aec58ed5fa090b8783493ceb3961384
Cristian Ponce <christian.rponce@gmail.com>
2015-10-18 23:55:33 -0700

    grid stuff

--- ProjectBit3D/ProjectBit/Assets/Scripts/GridGenerator.cs
+++ ProjectBit3D/ProjectBit/Assets/Scripts/GridGenerator.cs
@@ -2,12 +2,28 @@ using UnityEngine;
 
 public class GridGenerator : MonoBehaviour
 {
+    //grid
     public GameObject grid;
-    public GameObject tilePrefab;
+    //tile prefab
+    public GameObject lightTile;
+    public GameObject darkTile;
+    //refrence plane;
     GameObject plane;
 
-    public float width = 10;
-    public float height = 10;
+    public int gridsize = 10;
+
+    //Offset to make them spawn at the psoition of the actual grid but I think they are better off staying at 0
+    //We'll talk about it later
+    /*
+    float zoffset = 0.5f;
+    float yoffset = -0.5f;
+    float xoffset = 0.5f;
+    */
+
+    float zoffset = 0;
+    float yoffset = -0.5f;
+    float xoffset = 0;
+
     [HideInInspector]
     public bool gridGenerated = false;
     
@@ -17,6 +33,46 @@ public class GridGenerator : MonoBehaviour
     }
 
     public void GenerateGrid()
+    {
+        for (int i = 0; i < gridsize; i++)
+        {
+            if(i%2==0)
+                plane = (GameObject)Instantiate(lightTile, new Vector3(xoffset + i, yoffset, zoffset), Quaternion.identity);
+            else
+                plane = (GameObject)Instantiate(darkTile, new Vector3(xoffset + i, yoffset, zoffset), Quaternion.identity);
+            plane.transform.SetParent(grid.transform);
+            for (int j = 1; j < gridsize; j++)
+            {
+                if (i % 2 == 0)
+                {
+                    if(j%2==0)
+                    {
+                        plane = (GameObject)Instantiate(lightTile, new Vector3(xoffset + i, yoffset, zoffset + j), Quaternion.identity);
+                    }
+                    else 
+                    {
+                        plane = (GameObject)Instantiate(darkTile, new Vector3(xoffset + i, yoffset, zoffset + j), Quaternion.identity);
+                    }
+                    plane.transform.SetParent(grid.transform);
+                }
+                else
+                {
+                    if (j % 2 == 0)
+                    {
+                        plane = (GameObject)Instantiate(darkTile, new Vector3(xoffset + i, yoffset, zoffset + j), Quaternion.identity);
+                    }
+                    else
+                    {
+                        plane = (GameObject)Instantiate(lightTile, new Vector3(xoffset + i, yoffset, zoffset + j), Quaternion.identity);
+                    }
+                    plane.transform.SetParent(grid.transform);
+                }
+            }
+            gridGenerated = !gridGenerated;
+        }
+    }
+    /*
+    public void GenerateGridOld()
     {
         int x = 0;
         int z = 0;
@@ -35,4 +91,5 @@ public class GridGenerator : MonoBehaviour
         }
         gridGenerated = !gridGenerated;
     }
+    */
 }
